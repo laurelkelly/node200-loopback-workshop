4e886a3a3ce2e31ed9923f92e370afa13d1c6cc7
Laurel Kelly <laurellkelly@gmail.com>
2020-11-24 09:58:16 -0800

    Initial commit

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,11 +1,9 @@
 module.exports = {
     "MongoDB": {
-        "port": 27017,
         "name": "MongoDB",
         "connector": "mongodb",
         "url": process.env.MONGODB_URL,
-        "database": ""
-        // 'useNewUrlParser': true, 
-        // 'useUnifiedTopology': true
+        'useNewUrlParser': true, 
+        'useUnifiedTopology': true
     }
 }
