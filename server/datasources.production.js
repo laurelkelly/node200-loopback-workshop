ac233c601068e857435f28ae9aecefaf28686767
Laurel Kelly <laurellkelly@gmail.com>
2020-11-24 15:08:54 -0800

    Updates package.json

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -7,18 +7,3 @@ module.exports = {
         "url": process.env.MONGODB_URL
     }
 }
-
-// 'use strict';
-// const envs = require('./env.js');
-
-// module.exports = {
-//     mongoatlas: {
-//         url: envs.url,
-//         port: 27017,
-//         database: "db",
-//         name: "mongoatlas",
-//         connector: "mongodb",
-//         user: envs.user,
-//         password: envs.password
-//     }
-// }
