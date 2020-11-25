3cd6fb7e1827eb7af41076d63be2acd916882116
Laurel Kelly <laurellkelly@gmail.com>
2020-11-24 16:42:52 -0800

    Updates datasources.local.js

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,17 +1,24 @@
+require('dotenv').config();
+
 module.exports = {
     "db": {
         "name": "db",
         "connector": "memory"
     },
+    // "MongoDB": {
+    //     "host": "",
+    //     "port": 0,
+    //     "url": "mongodb://localhost:27017",
+    //     "database": "",
+    //     "password": "",
+    //     "name": "MongoDB",
+    //     "user": "",
+    //     "useNewUrlParser": true,
+    //     "connector": "mongodb"
+    // }
     "MongoDB": {
-        "host": "",
-        "port": 0,
-        "url": "mongodb://localhost:27017",
-        "database": "",
-        "password": "",
         "name": "MongoDB",
-        "user": "",
-        "useNewUrlParser": true,
-        "connector": "mongodb"
+        "connector": "mongodb",
+        "url": process.env.MONGODB_URL
     }
 }
